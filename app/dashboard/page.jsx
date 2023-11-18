037d60954deffdeda94237ab1aa2e4b14c79659a
Zyphr <142991851+CraZyphr@users.noreply.github.com>
2023-11-18 23:42:36 +0530

    2nd commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,30 @@
+
+
+import styles from "../ui/dashboard/dashboard.module.css";
+
+
+import Card from "./card/card";
+import Chart from "./chart/chart";
+import Rightbar from "./rightbar/rightbar";
+import Transactions from "./transactions/transactions";
+
 const Dashboard = () => {
-    return (
-      <div>Dashboard</div>
-    )
-  }
-  
-  export default Dashboard
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
