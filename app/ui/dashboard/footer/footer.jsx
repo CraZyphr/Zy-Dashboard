1b3a7fbaa21abf565edd7711b12f2e6a408ed8f7
Zyphr <142991851+CraZyphr@users.noreply.github.com>
2023-11-19 00:57:50 +0530

    3rd commit

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+const Footer = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.logo}>Niladri Howladar</div>
+        <div className={styles.text}>© All Right Reserved</div>
+      </div>
+    )
+  }
+  
+  export default Footer
